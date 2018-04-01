c43705cf7bd69a69013914f9cf2efe5c224aa12c
Rocky Lee <sangrakl@gmail.com>
2018-04-01 13:37:48 +0900

    'change'

--- rockyleekr/wsgi.py
+++ rockyleekr/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for programmers project.
+WSGI config for rockyleekr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "programmers.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rockyleekr.settings")
 
 application = get_wsgi_application()
